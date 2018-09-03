d7d2db95dd92efd4e63a994c1a09886101d19fc1
QuentinMcCarthy <845868401@bun2-206-13.local>
2018-09-04 11:22:48 +1200

    Node back-end setup

---
+++ node/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(function(req,res,next){
+	console.log(`${req.method} request for ${req.url}`);
+	next();
+});
+
+app.set(`port`, (process.env.PORT || 4000));
+
+app.listen(app.get(`port`), () => { console.log(`Server is running on port ${app.get(`port`)}`) });
