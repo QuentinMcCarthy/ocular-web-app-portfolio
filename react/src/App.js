4ee7b6b0557e357d3e78e1c822a28cb990a1f064
sumitramanga <sumitramanga@gmail.com>
2018-09-11 10:56:22 +1200

    Testing base

--- react/src/App.js
+++ react/src/App.js
@@ -5,24 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="container">
-
-        {/* Nav */}
-        <div className="header fixed-top">
-
-          <div className="headerBar">
-            <div className="logo">OCULAR</div>
-            <button type="button" name="button btn-primary" className="btn btn-primary menuBtn"><i className="fas fa-bars fa-3x"></i>Menu</button>
-          </div>
-
-          <ol className="menu">
-            <li className="menuItem">Home</li>
-            <li className="menuItem">Designers</li>
-            <li className="menuItem">Contact</li>
-          </ol>
-
-        </div>
-      </div>
+        <div></div>
     );
   }
 }
