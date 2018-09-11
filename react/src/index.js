e3e5659e208c4e240b910f6c68fc283becd80b06
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-09-12 10:10:55 +1200

    Moved React App.js into index.js and fixed resulting errors

---
+++ react/src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+import './css/index.min.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div></div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
