4bab5f76763935340ccfbd62047c3fbd319ac071
sumitramanga <sumitramanga@gmail.com>
2018-09-12 11:47:01 +1200

    Created Our Latest Projects section template and added styling

--- react/public/js/script.js
+++ react/public/js/script.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
   $(".scroll-down").click(function(){
       $("html, body").animate({
-          scrollTop:$(".our-goals").offset().top
+          scrollTop:$(".our-goal").offset().top
       }, 3000);
   });
 
