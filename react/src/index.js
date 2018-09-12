8a9975b82683245eb78d67fdb08269f2b55f5ea1
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-09-13 11:04:43 +1200

    Replace all double quotes with single quotes and fix camelCase

--- react/src/index.js
+++ react/src/index.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import ReactDOM from "react-dom";
-// import { Chart } from "react-google-charts";
-// import "./css/index.min.css";
-import "./css/index.css";
+import ReactDOM from 'react-dom';
+// import { Chart } from 'react-google-charts';
+// import './css/index.min.css';
+import './css/index.css';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
-				<div className="designer-profile position-relative">
-					<div className="splitv-third w-100"></div>
-					<div className="splitv-third w-100 d-flex align-items-center flex-column">
-						<div className="profile-image h-100">
+				<div className='designer-profile position-relative'>
+					<div className='splitv-third w-100'></div>
+					<div className='splitv-third w-100 d-flex align-items-center flex-column'>
+						<div className='profile-image h-100'>
 
 						</div>
 					</div>
-					<div className="splitv-third w-100"></div>
+					<div className='splitv-third w-100'></div>
 				</div>
 			</div>
 		);
 	}
 }
 
-ReactDOM.render(<App />, document.getElementById("desprofiles"));
+ReactDOM.render(<App />, document.getElementById('desProfiles'));
