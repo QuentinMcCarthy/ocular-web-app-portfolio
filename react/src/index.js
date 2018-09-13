1e109e87eed2f6268e3513d02a93650d00b58691
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-09-14 10:59:05 +1200

    Moved front-end node_modules to React

--- react/src/index.js
+++ react/src/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-// import { Chart } from 'react-google-charts';
+import { Chart } from 'react-google-charts';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/jquery/dist/jquery.min.js';
+import '../node_modules/popper.js/dist/umd/popper.min.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import '../node_modules/google-maps/lib/Google.min.js';
 // import './css/index.min.css';
 import './css/index.css';
 
