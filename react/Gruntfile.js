f963aa71dbe6a3010bbc68541084432972663da6
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-09-14 11:48:34 +1200

    Attempting to fix fatal errors

---
+++ react/Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function(grunt){
+	require('time-grunt')(grunt);
+	require('jit-grunt')(grunt);
+
+	grunt.initConfig({
+		sass: {
+			src: {
+				files: {
+					"src/css/index.css": [
+						"src/sass/index.scss",
+						"src/sass/index.css"
+					]
+				}
+			}
+		},
+		cssmin: {
+			src: {
+				files: [{
+					expand: true,
+					cwd: 'src/css',
+					src: ['*.css', '!*.min.css'],
+					dest: 'src/css',
+					ext: '.min.css'
+				}]
+			}
+		},
+		jshint: {
+			public: ['rc/js/script.js']
+		},
+		uglify: {
+			public: {
+				files: {
+					'src/js/script.min.js': ['src/js/script.js']
+				}
+			}
+		},
+		watch: {
+			pubsass: {
+				files: ['public/sass/master.scss'],
+				tasks: ['sass']
+			},
+			srcsass: {
+				files: ['src/sass/*.scss'],
+				tasks: ['sass']
+			},
+			// pubcss: {
+			// 	files: ['public/css/master.css'],
+			// 	tasks: ['cssmin']
+			// },
+			// srccss: {
+			// 	files: ['src/css/*.css', '!src/css/*.min.css'],
+			// 	tasks: ['cssmin']
+			// },
+			// pubjs: {
+			// 	files: ['public/js/script.js'],
+			// 	tasks: ['jshint', 'uglify']
+			// }
+		}
+	});
+
+	// grunt.loadNpmTasks();
+
+	// grunt.registerTask();
+};
--- react/src/js/script.js
+++ react/src/js/script.js
@@ -1,81 +1,81 @@
-$(document).ready(function(){
-	$('.menu-btn').click(function(){
-		$('.menu').toggle();
-	});
-	$('.menu-btn').click(function(){
-		$('.menu-btn').toggleClass('btnMenuClicked');
-	});
-
-	// Temporary navigation
-	$('#navIndex').click(function(){
-		$('#sectIndex').show();
-		$('#sectDesigners').hide();
-        $('#sectContact').hide();
-        $('#menu').hide();
-	});
-
-	$('#navDesigners').click(function(){
-        $('#sectDesigners').show();
-        $('#sectIndex').hide();
-        $('#sectContact').hide();
-        $('#nav').hide();
-	});
-
-    $('#navContact').click(function(){
-        $('#sectContact').show();
-		$('#sectIndex').hide();
-        $('#sectDesigners').hide();
-        $('#menu').hide();
-    });
-
-    $('.scroll-down').click(function(){
-        $('html, body').animate({
-            scrollTop:$('.our-goal').offset().top
-        }, 3000);
- 	});
-
-	$('.scroll-up').click(function(){
-        $('html, body').animate({
-            scrollTop:$('.banner').offset().top
-        }, 3000);
- 	});
-
-	// View stats button
-	$('#viewStats').click(function() {
-		$('#sectDesigners').hide();
-		$('.header-bar').hide();
-		$('#secStatistics').show();
-	})
-
-	$.ajax({
-		type: 'GET',
-		url: 'http://192.168.33.10:4000/data/staff.json',
-		dataType: 'json',
-		success: function(data){
-			console.log(data);
-
-			var initProfile = data[0].behance;
-
-			$.ajax({
-				type: 'GET',
-				url: 'http://192.168.33.10:4000/behance/user/'+initProfile+'/projects',
-				dataType: 'json',
-				success: function(projData){
-					console.log(projData);
-
-					setProfileBackground(projData.projects[0].covers.original);
-				},
-				error: function(projErr){
-					console.log('Error '+projErr.status);
-					console.log(projErr);
-				}
-			});
-		},
-		error: function(err){
-			console.log('Error '+err.status);
-			console.log(err);
-		}
-	});
+jQuery(document).ready(function(){
+	// $('.menu-btn').click(function(){
+	// 	$('.menu').toggle();
+	// });
+	// $('.menu-btn').click(function(){
+	// 	$('.menu-btn').toggleClass('btnMenuClicked');
+	// });
+	//
+	// // Temporary navigation
+	// $('#navIndex').click(function(){
+	// 	$('#sectIndex').show();
+	// 	$('#sectDesigners').hide();
+    //     $('#sectContact').hide();
+    //     $('#menu').hide();
+	// });
+	//
+	// $('#navDesigners').click(function(){
+    //     $('#sectDesigners').show();
+    //     $('#sectIndex').hide();
+    //     $('#sectContact').hide();
+    //     $('#nav').hide();
+	// });
+	//
+    // $('#navContact').click(function(){
+    //     $('#sectContact').show();
+	// 	$('#sectIndex').hide();
+    //     $('#sectDesigners').hide();
+    //     $('#menu').hide();
+    // });
+	//
+    // $('.scroll-down').click(function(){
+    //     $('html, body').animate({
+    //         scrollTop:$('.our-goal').offset().top
+    //     }, 3000);
+ 	// });
+	//
+	// $('.scroll-up').click(function(){
+    //     $('html, body').animate({
+    //         scrollTop:$('.banner').offset().top
+    //     }, 3000);
+ 	// });
+	//
+	// // View stats button
+	// $('#viewStats').click(function() {
+	// 	$('#sectDesigners').hide();
+	// 	$('.header-bar').hide();
+	// 	$('#secStatistics').show();
+	// });
+	//
+	// $.ajax({
+	// 	type: 'GET',
+	// 	url: 'http://192.168.33.10:4000/data/staff.json',
+	// 	dataType: 'json',
+	// 	success: function(data){
+	// 		console.log(data);
+	//
+	// 		var initProfile = data[0].behance;
+	//
+	// 		$.ajax({
+	// 			type: 'GET',
+	// 			url: 'http://192.168.33.10:4000/behance/user/'+initProfile+'/projects',
+	// 			dataType: 'json',
+	// 			success: function(projData){
+	// 				console.log(projData);
+	//
+	// 				setProfileBackground(projData.projects[0].covers.original);
+	// 			},
+	// 			error: function(projErr){
+	// 				console.log('Error '+projErr.status);
+	// 				console.log(projErr);
+	// 			}
+	// 		});
+	// 	},
+	// 	error: function(err){
+	// 		console.log('Error '+err.status);
+	// 		console.log(err);
+	// 	}
+	// });
 });
 
 function setProfileBackground(image){
