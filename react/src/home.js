a72f47cafa39d121db4fb22478bd81b418e925dd
Jiah Kwon <itisgia@gmail.com>
2018-09-15 18:25:58 +1200

    map added

--- react/src/home.js
+++ react/src/home.js
@@ -16,20 +16,21 @@ class Home extends Component {
 	                </div>
 	            </div> {/* banner ends */}
 
-	            <div className='container'>
-	                {/* Our goal section */}
-	                <div className='our-goal'>
-						<div className='row'>
-							<div className='col-sm'>
-								<h2 className='second-heading'>Our Goal</h2>
-								<hr className='title-hr'/>
-							</div>
-							<div className='col-sm'>
-		                    	<p className='body-text our-goal-text'>Our goal is to provide our clients with an expectional outcome with a insightful and indepth process. We care about our clients wants and needs whilst catering to the audience their business attracts</p>
-							</div>
-						</div>
-	                </div>
 
+                {/* Our goal section */}
+                <div className='our-goal'>
+					<div className='row'>
+						<div className='col-sm'>
+							<h2 className='second-heading'>Our Goal</h2>
+							<hr className='title-hr'/>
+						</div>
+						<div className='col-sm'>
+	                    	<p className='body-text our-goal-text'>Our goal is to provide our clients with an expectional outcome with a insightful and indepth process. We care about our clients wants and needs whilst catering to the audience their business attracts</p>
+						</div>
+					</div>
+                </div>
+
+				<div className='container'>
 	                {/* Our latest projects section */}
 	                <h2 className='second-heading'>Our Latest Projects</h2>
 	                <hr className='title-hr latest-projects-hr'/>
