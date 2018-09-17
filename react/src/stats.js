88a49a76ad971d61ec8149cf5557b6056d4df951
sumitramanga <sumitramanga@gmail.com>
2018-09-18 11:25:30 +1200

    Created function for closing stats section. In progress of setting state.

--- react/src/stats.js
+++ react/src/stats.js
@@ -2,15 +2,39 @@ import React, { Component } from 'react';
 
 
 class Stats extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			viewStatsOpen: { display: 'block' }
+		}
+		this.closeStats = this.closeStats.bind(this);
+	}
 	render() {
 		return(
 			<div id='sectStatistics' className='sect-statistics container' style={this.props.viewStatsOpen}>
-				<button type='button' className='close-btn'><i className='fas fa-times fa-2x'></i></button>
+				<button type='button' className='close-btn' onClick={this.closeStats}>
+					<i className='fas fa-times fa-2x'></i>
+				</button>
 				<h1 className='second-heading stats-heading'>Darryl's Projects Statistics</h1>
 				<hr className='title-hr'/>
 				<div id='googleChart' className='chart-stats'></div>
 			</div>
 		)
+	}
+
+	closeStats(e){
+		e.preventDefault();
+		console.log('working');
+		console.log(this.props.viewStatsOpen);
+		if (this.state.viewStatsOpen.display === 'block') {
+			this.setState({
+				viewStatsOpen: { display: 'none'}
+			});
+			console.log('closing');
+			console.log(this.props.viewStatsOpen);
+		} else {
+			console.log('Not closing');
+		}
 	}
 }
 
