473b025d712339028037af3633a7056bb0e8cc47
sumitramanga <sumitramanga@gmail.com>
2018-09-17 21:12:01 +1200

    Mobile and tablet version of stats section complete.

--- react/src/stats.js
+++ react/src/stats.js
@@ -4,9 +4,9 @@ import React, { Component } from 'react';
 class Stats extends Component {
 	render() {
 		return(
-			<div id='sectStatistics' className='sectStatistics container' style={this.props.viewStatsOpen}>
+			<div id='sectStatistics' className='sect-statistics container' style={this.props.viewStatsOpen}>
 				<button type='button' className='close-btn'><i className='fas fa-times fa-2x'></i></button>
-				<h1 className='second-heading statsHeading'>Darryl's Projects Statistics</h1>
+				<h1 className='second-heading stats-heading'>Darryl's Projects Statistics</h1>
 				<hr className='title-hr'/>
 				<div id='googleChart' className='chart-stats'><img src='https://placehold.it/280x230' alt='place holder'></img></div>
 			</div>
